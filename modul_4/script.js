77dab3e629f16c8f07f959a3690f8d3b22878231
Alexander Almas S <xanderalmas@gmail.com>
2024-03-14 12:39:16 +0700

    tambah modul baru (kalkulator bagus)

---
+++ modul_4/script.js
@@ -0,0 +1,30 @@
+const button = document.querySelectorAll("button");
+const result = document.getElementById("result");
+
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", () => {
+        const buttonValue = button[i].textContent;
+        // if ()
+        if (buttonValue === "C") {
+            clearResult();
+        }
+        else if (buttonValue === "=") {
+            calculateResult();
+        }
+        else {
+            appendValue(buttonValue);
+        }
+    });
+}
+
+function clearResult() {
+    result.value = "";
+}
+
+function calculateResult() {
+    result.value = eval(result.value); // eval javascript
+}
+
+function appendValue(buttonValue) {
+    result.value = result.value + buttonValue;
+}
